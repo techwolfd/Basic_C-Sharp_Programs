3adefa9371c7b3abef9683f0fabad7096c5c0beb
techwolfd <93291605+techwolfd@users.noreply.github.com>
2021-11-30 12:00:20 -0800

    infomrediario

---
+++ Basic_C#_Programs/InformeDiario/InformeDiario/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InformeDiario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {//saludo de introduccion
+            Console.WriteLine("The Tech Academy \n");
+            Console.WriteLine("Informe diario (DR) del estudiante \n");
+
+            Console.WriteLine("a. Como te llamas:");//peticion y almacenado del nombre en un tipo string
+            string nombre = Console.ReadLine();
+
+            Console.WriteLine("b. En que curso estas:");
+            string nombreCurso = Console.ReadLine();
+
+            Console.WriteLine("c. que numero de pagina :");
+            string numeroPagina = Console.ReadLine();// se guarda el numero de pagina
+            // type short 
+
+            Console.WriteLine("d. necesita ayuda (responder verdadero/falso) :");
+            string wantayuda = Console.ReadLine();//tipo tipo booleano 
+            // typebool
+
+            Console.WriteLine("e. hubo alnua experiencia positiva que le gustaria compartir?");
+            string experiencia= Console.ReadLine();
+
+            Console.WriteLine("f. hay otro comentario:");
+            string comentario = Console.ReadLine();
+
+            Console.WriteLine("g. cuantas horas estudiaste hoy");
+            string horasEstudio = Console.ReadLine();//se guarda las horas estudiadad
+            // typebyte
+
+            Console.WriteLine("Gracias por sus respuestas. \" \n \"Un instructor responderá a esto en breve. \"\n Qué tengas un lindo día!\"\n \" Este es el final del programa.");
+
+        }
+    }
+}
+
+
+//using System;
+
+//namespace myConsoleProject.cs
+//{
+//    class Program
+//    {
+//        static void Main()
+//        {
+//            Console.WriteLine("Cual es tu nombre?");
+//            string name = Console.ReadLine();
+
+//            Console.WriteLine("Hola, " + name + "! y ejemplo de uso de dobles \" comillas \" ");
+
+//            Console.WriteLine("bienvenido a acme accounting systems");
+//            Console.WriteLine("recuerda, nosotros \"contamos\" con usted");
+
+//            Console.WriteLine("cual es tu numero favorito?");
+//            string favoriteNumber = Console.ReadLine();
+//            int favNum = Convert.ToInt32(favoriteNumber);
+//            int total = favNum + 10;
+//            Console.WriteLine("tu numero favorito mas 10 es :" + total);
+
+//            bool isStuding = false;
+//            Console.WriteLine(isStuding);
+//            byte hoursworked = 42;
+//            Console.WriteLine(hoursworked);
+//            sbyte currentTemperature = -13;
+//            Console.WriteLine(currentTemperature);
+//            char questionMar = '\u2103';
+//            Console.WriteLine(questionMar);
+
+//            decimal moneyInBank = 100.5m;
+//            Console.WriteLine(moneyInBank);
+//            double heightCentimeters = 211.30202092;
+//            Console.WriteLine(heightCentimeters);
+
+//            float secondsleft = 2.62f;
+//            Console.WriteLine(secondsleft);
+//            short temperatureOnMar = -341;
+//            Console.WriteLine(temperatureOnMar);
+//            string myname = "wolf";
+//            Console.WriteLine(myname);
+
+//            int currentAge = 30;
+//            string yearsOld = currentAge.ToString();
+//            Console.WriteLine(currentAge);
+
+//            bool isRaining = true;
+//            Console.WriteLine(isRaining);
+//            string rainingStatus = Convert.ToString(isRaining);
+
+//            Console.WriteLine(rainingStatus);
+
+//            Console.ReadLine();
+//        }
+//    }
+//}
--- Basic_C#_Programs/myConsoleProject.cs/myConsoleProject.cs/Program.cs
+++ Basic_C#_Programs/myConsoleProject.cs/myConsoleProject.cs/Program.cs
@@ -9,11 +9,49 @@ namespace myConsoleProject.cs
             Console.WriteLine("Cual es tu nombre?");
             string name = Console.ReadLine();
 
-            Console.WriteLine("Hola, " + name + "! usando \" comillas \" ");
+            Console.WriteLine("Hola, " + name + "! y ejemplo de uso de dobles \" comillas \" ");
 
             Console.WriteLine("bienvenido a acme accounting systems");
             Console.WriteLine("recuerda, nosotros \"contamos\" con usted");
-            Console.Read();
+            
+            Console.WriteLine("cual es tu numero favorito?");
+            string favoriteNumber = Console.ReadLine();
+            int favNum = Convert.ToInt32(favoriteNumber);
+            int total = favNum + 10;
+            Console.WriteLine("tu numero favorito mas 10 es :" + total);
+
+            bool isStuding = false;
+            Console.WriteLine(isStuding);
+            byte hoursworked = 42;
+            Console.WriteLine(hoursworked);
+            sbyte currentTemperature = -13;
+            Console.WriteLine(currentTemperature);
+            char questionMar = '\u2103';
+            Console.WriteLine(questionMar);
+
+            decimal moneyInBank = 100.5m;
+            Console.WriteLine(moneyInBank);
+            double heightCentimeters = 211.30202092;
+            Console.WriteLine(heightCentimeters);
+
+            float secondsleft = 2.62f;
+            Console.WriteLine(secondsleft);
+            short temperatureOnMar = -341;
+            Console.WriteLine(temperatureOnMar);
+            string myname = "wolf";
+            Console.WriteLine(myname);
+
+            int currentAge = 30;
+            string yearsOld = currentAge.ToString();
+            Console.WriteLine(currentAge);
+
+            bool isRaining = true;
+            Console.WriteLine(isRaining);
+            string rainingStatus = Convert.ToString(isRaining);
+
+            Console.WriteLine(rainingStatus);
+
+            Console.ReadLine();
         }
     }
 }
