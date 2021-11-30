e3bf9c3cab68a95c242af6bfcdf63e80474528db
techwolfd <93291605+techwolfd@users.noreply.github.com>
2021-11-30 09:27:51 -0800

    sharp

---
+++ Basic_C#_Programs/myConsoleProject.cs/myConsoleProject.cs/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace myConsoleProject.cs
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Cual es tu nombre?");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Hola, " + name + "! usando \" comillas \" ");
+
+            Console.WriteLine("bienvenido a acme accounting systems");
+            Console.WriteLine("recuerda, nosotros \"contamos\" con usted");
+            Console.Read();
+        }
+    }
+}
