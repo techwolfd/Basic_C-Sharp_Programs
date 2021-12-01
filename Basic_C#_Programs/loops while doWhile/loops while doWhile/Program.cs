b89992c4252ee4a39ee1a1b2ccd02e3b8935d68e
techwolfd <93291605+techwolfd@users.noreply.github.com>
2021-11-30 17:54:32 -0800

    loop

--- Basic_C#_Programs/loops while doWhile/loops while doWhile/Program.cs
+++ Basic_C#_Programs/loops while doWhile/loops while doWhile/Program.cs
@@ -18,14 +18,11 @@ namespace loops_while_doWhile
             Console.WriteLine("\n loop while de secuancia de numeros del 0 al 5" );
             
             int i = 0;
-            bool whileloop = i ==1;//condicional para que whileloop sea true
-
-            while (!whileloop)//uso de while
+            while (i < 6)//uso de while
             {
                 Console.WriteLine(i);//imprime numero i
                 i++;
-                if (i == 6) //condicion 
-                    break;
+               
             }
             Console.WriteLine("\n loop  do while de secuancia de numeros del 0 al 5");
 
@@ -34,36 +31,12 @@ namespace loops_while_doWhile
             {
                 Console.WriteLine(j);// imprecion de j
                 j++;// incremento de j
-                if (j == 6)// condicion para j terminar
-                    break;
+               
             }
-            while (!whileloop); // hacer hasta que se cumpla la condicion
+            while (j < 6); // hacer hasta que se cumpla la condicion
 
             Console.ReadLine();
 
-
-            //if (pesoPaquete > 50)
-            //{
-            //    Console.WriteLine("\nerror : Paquete demasiado pesado para ser enviado a través de Package Express. \nQue tenga un buen día.");
-            //}
-
-            //Console.WriteLine("\nIngrese el ancho del paquete");
-            //int anchoPaquete = Convert.ToInt32(Console.ReadLine());//variable donde se guarda el ancho del paquete
-
-            //Console.WriteLine("\nIngrese la altura del paquete");
-            //int alturaPaquete = Convert.ToInt32(Console.ReadLine());//variable donde se guarda el ancho del paquete
-
-            //Console.WriteLine("\nIngrese el longitud del paquete");
-            //int longitudPaquete = Convert.ToInt32(Console.ReadLine());//variable donde se guarda el ancho del paquete
-
-            //if ((anchoPaquete + alturaPaquete + longitudPaquete) > 50) // se condiciona si la suma de las dimensiones es mayor a 50
-            //{
-            //    Console.WriteLine("\nerror : Paquete demasiado grande para enviarse a través de Package Express. \nQue tenga un buen día.");
-            //}
-
-            //int dimensiones = ((anchoPaquete * alturaPaquete * longitudPaquete) * pesoPaquete) / 100; //se realiza la operacion para obtener la cotizacion
-
-            //Console.WriteLine("la cotizacion es: $" + dimensiones + " dolares. Gracias!"); // se imprime el resultado
 
             //Console.ReadLine();
         }
